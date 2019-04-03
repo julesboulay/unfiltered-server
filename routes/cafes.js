d8fe00becb15fa651779c50a76bffc2c44b482db
julesboulay <jules.boulaydetouchet@mail.mcgill.ca>
2019-04-03 19:56:11 +0800

    Now using next for error handling in expres

--- routes/cafes.js
+++ routes/cafes.js
@@ -1,28 +1,26 @@
 const Cafe = require("../database/cafe_query");
 
 module.exports = (app, connection) => {
-  app.get("/cafes", async (req, res) => {
+  app.get("/cafes", async (req, res, next) => {
     var { lat, lng, diff } = req.query;
     if (lat === undefined || lng === undefined) {
-      res.error("No location selected.");
-      return;
+      next("No location selected.");
     }
     if (isNaN(lat) || isNaN(lng)) {
-      res.error("Location not a number.");
-      return;
+      next("Location not a number.");
     }
 
     var query = Cafe.getCafesQuery(Number(lat), Number(lng), Number(diff));
     await connection.query(query, function(error, result) {
       if (error) {
-        res.error(error);
+        next(error);
       } else {
         res.send(result);
       }
     });
   });
 
-  app.post("/cafes", async (req, res) => {
+  app.post("/cafes", async (req, res, next) => {
     var { google_place_id, place_name, lat, lng, address } = req.body;
     var query = `
     INSERT INTO Cafe (
@@ -41,7 +39,7 @@ module.exports = (app, connection) => {
 
     await connection.query(query, function(error, result) {
       if (error) {
-        res.error(error);
+        next(error);
       } else {
         res.send(result);
       }
